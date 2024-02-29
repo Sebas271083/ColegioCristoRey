1f734e646cac0343491cc3c7f2906c2248dcb768
Delgado Sebastian <sebdelgado83@gmail.com>
2024-02-28 23:42:39 -0300

    Routes y Controllers

--- backend/controllers/contacto.js
+++ backend/controllers/contacto.js
@@ -12,14 +12,3 @@ export const contacto = (req, res) => {
     console.log(indexPath);
   };
 
-  export const index = (req, res) => {
-    const indexPath = join(__dirname, `../../frontend/public/page/index.html`);
-    const headerPath = join(__dirname, `../../frontend/public/page/header.html`);
-
-    
-    // Send the file
-    res.sendFile(indexPath);
-    res.sendFile(headerPath);
-
-    console.log(indexPath);
-  }
